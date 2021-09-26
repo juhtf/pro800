342d235d914478c3222972fc0e46099eced761a4
juhtf <86907413+juhtf@users.noreply.github.com>
2021-09-26 13:29:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+   }
+
+
+
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("     ").style.display="block";
+	document.getElementById("sort").innerHTML=names_of_people;
+	document.getElementById("id=sort_button").innerHTML=names_of_people;
+	}
+
+
+function sorting()
+	{
+		names_of_people .sort();
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
+
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
+}
